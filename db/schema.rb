4c1d583a2baf4cb7b66341b46c62fdf23b1d33e7
Juan Mercedes <j.mercedes@thedesignpill.com>
2014-01-21 08:51:47 -0400

    add assets

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140121115654) do
+ActiveRecord::Schema.define(version: 20140121120903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "results", force: true do |t|
+    t.string   "patient"
+    t.integer  "age"
+    t.string   "gender"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
