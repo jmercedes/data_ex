1a32d130cb86683e09b442bd29c069c4a524fcc6
Juan Mercedes <j.mercedes@thedesignpill.com>
2014-01-21 15:19:18 -0400

    add env variables for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 CmpcLabDelta::Application.initialize!
 
-
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
-# Initialize the Rails application.
-CmpcLabV1::Application.initialize!
-
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
