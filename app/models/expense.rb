16e4cd519b50a47b87013d20f24206ef9189753a
Juan Mercedes <j.mercedes@thedesignpill.com>
2014-02-27 20:09:29 -0400

    add validations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,31 @@
 class Expense < ActiveRecord::Base
   
+   validates :ncf, length: { is: 19 }
+   #validates_with  validate_businessid, :businessid
+   
+   def self.validate_businessid
+     if businessid.length == 9 || 11
+       true
+     else
+       false
+     end
+   end
+   
+   def self.validate_document(typeid)
+     if businessid.length == 9
+       typeid == 1
+       else
+         typeid == 2
+     end 
+   end
+   
+   validates :modified_document, length: { is: 19 }
+   validates :billed_itbis, numericality: true
+   validates :retained_itbis, numericality: true
+   validates :amount_billed, numericality: true
+   validates :retained_isr, numericality: true
+  
+  
   def self.to_csv(options = {})
     CVS.generate(options) do |csv|
       csv << column_names
